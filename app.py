60aba791552676d1930754775096480da7e356ee
Ahmed AbdelKhalek <abdelkhaleqahmed625@gmail.com>
2026-02-15 19:52:05 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import base64
 import io
+import cv2
+import os
 import numpy as np
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from PIL import Image
 import uvicorn
+from img2segment import img2segmnt
 
-# 1. تعريف شكل البيانات (Base64 + Coordinates)
+# (Base64 + Coordinates)
 class SegmentRequest(BaseModel):
     image_base64: str
     x: int
@@ -19,19 +22,34 @@ async def segment_image(request: SegmentRequest):
     try:
         encoded_data = request.image_base64.split(",")[-1]
         image_data = base64.b64decode(encoded_data)
-        image = Image.open(io.BytesIO(image_data)).convert("RGB")
+        # image = Image.open(io.BytesIO(image_data)).convert("RGB")
+        temp_filename = "temp_input.png"
         
+        with open(temp_filename, "wb") as f:
+            f.write(image_data)
         
-        width, height = image.size
-        if request.x >= width or request.y >= height:
-            raise HTTPException(status_code=422, detail=f"Coordinates out of bounds. Image size: {width}x{height}")
+        # width, height = image.size
+        # if request.x >= width or request.y >= height:
+        #     raise HTTPException(status_code=422, detail=f"Coordinates out of bounds. Image size: {width}x{height}")
+        
+        image_pil = Image.open(io.BytesIO(image_data)).convert("RGB")
+        source_image_np = np.array(image_pil)
+        
+        input_point = [request.x, request.y]
 
         # pixels = image.load()
         # pixels[request.x, request.y] = (255, 0, 0) 
-
+        segmented_np = img2segmnt(source_image_np, input_point)
+        
+        segmented_rgba = cv2.cvtColor(segmented_np, cv2.COLOR_BGRA_RGBA)
+        final_image = Image.fromarray(segmented_rgba)   
+             
         img_bytes = io.BytesIO()
-        image.save(img_bytes, format="PNG")
+        final_image.save(img_bytes, format="PNG")
         encoded_result = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
+        
+        if os.path.exists(temp_filename):
+            os.remove(temp_filename)
 
         return {
             "status": "success",
